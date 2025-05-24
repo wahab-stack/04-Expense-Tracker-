74324b8ff1bf5974ebed2c34d4dbb1ca9c7063ec
Wahab1437 <wahab.suit997@gmail.com>
2025-05-24 14:01:20 +0500

    Completed Expense Tracker

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const expenseList = document.getElementById("expense-list");
   const totalAmountDisplay = document.getElementById("total-amount");
 
-  let expenses = [];
+  let expenses =JSON.parse(localStorage.getItem('expenses')) || [];
   let totalAmount = calculateTotal();
+
+  renderExpenses()
 
   expenseForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -21,16 +23,47 @@ document.addEventListener("DOMContentLoaded", () => {
       };
       expenses.push(newExpense);
       saveExpensesTolocal();
+      renderExpenses()
+      updateTotal()
 
       // Clear Input
       expenseNameInput.value = "";
       expenseAmountInput.value = "";
     }
   });
+  function renderExpenses(){
+    expenseList.innerHTML = ""
+    expenses.forEach(expense => {
+        const li = document.createElement('li')
+        li.innerHTML = `
+        ${expense.name} - $${expense.amount}
+        <button data-id="${expense.id}">Delete</button>
+        `;
+        expenseList.appendChild(li)
+    })
+  }
 
-  function calculateTotal() {}
+  function calculateTotal() {
+    return expenses.reduce((sum, expense) => sum + expense.amount , 0)
+  }
 
   function saveExpensesTolocal() {
     localStorage.setItem("expense", JSON.stringify(expenses));
   }
+
+  function updateTotal(){
+    totalAmount = calculateTotal()
+    totalAmountDisplay.textContent = totalAmount.toFixed(2)
+  }
+
+  expenseList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+        const expenseId = parseInt(e.target.getAttribute('data-id'))
+        expenses = expenses.filter(expense => expense.id !== expenseId)
+       
+        saveExpensesTolocal();
+        renderExpenses();
+        updateTotal();
+    }
+  })
 });
